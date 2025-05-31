ae2fdffcd6a9d3003e3f087e2f5e33718c5e9e92
Snehalgumber143 <snehalgumber@gmail.com>
2025-05-31 21:22:22 +0530

    attendance page changes

--- home/models.py
+++ home/models.py
@@ -31,3 +31,10 @@ class Admin(models.Model):
     def __str__(self):
         return self.username
 
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=[('Present', 'Present'), ('Absent', 'Absent')])
+
+    def __str__(self):
+        return f"{self.student.name} - {self.status} on {self.date}"
